b963cbd44660bf87b5099e44d163aa39495fd36b
ballpanuwat25 <ballpanuwat25@gmail.com>
2025-05-29 15:25:44 +0700

    ready to deploy

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
+    $('.zodiac_content').hide();
+    $('.result_content').hide();
+    $('#myModal').hide();
+
     $('#cta').click(function() {
-        console.log("clicked");
+        $('.main_content').fadeOut(500, function() {
+            $('.zodiac_content').fadeIn(500);
+        });
+    });
+
+    $('.zodiac_item').click(function() {
+        var zodiacId = $(this).data('zodiac');
+        if(zodiacId != 'libra') {
+            console.log('You clicked on zodiac: ' + zodiacId);
+            $("#myModal").fadeIn(500);
+        } else {
+            $('.zodiac_content').fadeOut(500, function() {
+                $('.result_content').fadeIn(500);
+            });
+        }
+    });
+
+    $('#submit').click(function() {
+        $("#myModal").fadeOut(500);
     });
 });
