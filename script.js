ff8637b6eb00312e5a1ef3890f404b0132e8eb79
ballpanuwat25 <ballpanuwat25@gmail.com>
2025-05-29 15:56:52 +0700

    add backdoor

--- script.js
+++ script.js
@@ -10,19 +10,22 @@ $(document).ready(function() {
         });
     });
 
+    let count = 0;
     $('.zodiac_item').click(function() {
         var zodiacId = $(this).data('zodiac');
         if(zodiacId == 'libra') {
             $('.zodiac_content').fadeOut(500, function() {
                 $('#res2').fadeIn(500);
             });
-        } else if (zodiacId == 'gemini') {
+        } else if (zodiacId == 'gemini' && count >= 3) {
             $('.zodiac_content').fadeOut(500, function() {
                 $('#res1').fadeIn(500);
             });
         } else {
             $("#myModal").fadeIn(500);
         }
+
+        count++;
     });
 
     $('#submit').click(function() {
